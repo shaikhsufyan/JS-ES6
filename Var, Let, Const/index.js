53e26d641a3afd0f316a7143a334a12618921459
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-01-26 21:35:23 +0530

    Var Let and const in JS

---
+++ Var, Let, Const/index.js
@@ -0,0 +1,36 @@
+//  Var
+// When declare variable using var variables in JavaScript that are
+//  function-scoped. 
+
+// Let & Const 
+// when we decalre a variable using let and const store in block
+//  scope
+
+// var a ;
+// a = "sufyan"
+
+// let name = "Sufyan";
+// name = "shaikh"
+
+// const name = "Sufyan";
+
+
+ 
+// function myFun (){
+//     var x = "Sufyan";
+//     if(true){
+//         let a = "sk";
+//         const b = "code";
+//         console.log(x);
+//     }
+     
+// }
+// myFun();
+
+
+const a = "sufyan";
+if(true){
+const a = "shaikh";
+console.log(a);
+}
+console.log(a);
