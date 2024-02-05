05d9cd087b0ec469f7e350203afaaae06647d421
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-02-05 09:34:51 +0530

    undefined & notdefine

---
+++ Undefine,Notdefine/index.js
@@ -0,0 +1,13 @@
+// Undefined
+// In variable undefined is like a placeholder till not assigning
+//  a value
+
+// undefined property indicates that a variable has not been 
+// assigned a value, or not declared
+ 
+// var a;
+// console.log(a);
+ 
+// Not Define
+
+console.log(b);
